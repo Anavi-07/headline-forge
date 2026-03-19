c327d4755d21672c5db328ab5cb0439894040808
Anavi-07 <anaviraj201@gmail.com>
2026-03-19 14:22:34 +0530

    updated project

---
+++ HeadlineForge.py
@@ -0,0 +1,65 @@
+import random
+subjects =[
+    "Virat Kohli",
+    "Narendra Modi",
+    "A group of monkeys",
+     "A lazy programmer",
+    "An AI robot",
+    "A mysterious hacker",
+    "A startup founder",
+    "A YouTuber",
+    "A school teacher",
+    "A billionaire",
+    "A gamer",
+    "A politician",
+    "A scientist",
+    "A dog in a suit",
+    "A talking parrot",
+    "A fitness influencer",
+    "A tech company",
+    "A secret agent",
+    "An alien visitor",
+    "A college couple"
+]
+
+actions=[
+     "forgets their own speech",
+    "orders food at 3 AM",
+    "crashes a random party",
+    "starts dancing in public",
+    "argues with an AI",
+    "accidentally goes live",
+    "fails miserably but becomes famous",
+    "creates chaos on social media",
+    "overreacts to a meme",
+    "spends all money on food"
+]
+
+places or thing =[
+     "in Mumbai",
+    "in Delhi",
+    "at a college campus",
+    "on social media",
+    "on Instagram",
+    "on YouTube",
+    "during a live stream",
+    "at a press conference",
+    "inside a secret lab",
+    "in a startup office",
+    "at a wedding",
+    "during an interview",
+    "on a reality show",
+    "at a cricket stadium",
+    "in a classroom",
+    "during exams",
+    "at a tech event",
+    "in a viral video",
+    "on national television",
+    "at midnight",
+    "on the streets",
+    "in a luxury hotel",
+    "inside a gaming room",
+    "at a coding hackathon"
+]
+
+while True:
